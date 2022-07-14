735a6ac839188438432609bb7531e901035cb0a2
Vinicios <vinicios.schulze@compasso.com.br>
2022-07-14 16:41:01 -0300

    Add MapTo on components
    Importing custom components

--- ui.frontend/src/components/containers/MainContent/index.js
+++ ui.frontend/src/components/containers/MainContent/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import { MapTo } from '@adobe/aem-react-editable-components';
 import Button from '../../micro/Button';
 import ImageComponent from '../../micro/ImageComponent';
 
 import { Container, ButtonContainer, ImageContainer } from './styles';
 import ErrorMessage from '../../micro/ErrorMessage';
 
-function MainContent({ imgSrc, errorTitle, errorText, buttonText }) {
+const MainContentEditConfig = {
+  emptyLabel: 'Main Content',
+  isEmpty: function (props) {
+    const img = !props.imgSrc || props.imgSrc.trim().length < 1;
+    const title = !props.title || props.title.trim().length < 1;
+    const text = !props.text || props.text.trim().length < 1;
+    const button = !props.buttonText || props.buttonText.trim().length < 1;
+    return !props || img || title || text || button;
+  },
+};
+
+const MainContent = ({ imgSrc, errorTitle, errorText, buttonText }) => {
   return (
     <Container>
       <ImageContainer>
@@ -20,6 +31,9 @@ function MainContent({ imgSrc, errorTitle, errorText, buttonText }) {
       </div>
     </Container>
   );
-}
+};
 
-export default MainContent;
+export default MapTo('aem-react-error-challenge/components/main-content')(
+  MainContent,
+  MainContentEditConfig
+);
--- ui.frontend/src/components/micro/Header/index.js
+++ ui.frontend/src/components/micro/Header/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { MapTo } from '@adobe/aem-react-editable-components';
 import { StyledText } from './styles';
+
+const HeaderEditConfig = {
+  emptyLabel: 'Header',
+  isEmpty: function (props) {
+    return !props || !props.text || props.text.trim().length < 1;
+  },
+};
 
 const Header = ({ text }) => {
   return <StyledText>{text}</StyledText>;
 };
 
-export default Header;
+export default MapTo('aem-react-error-challenge/components/header')(
+  Header,
+  HeaderEditConfig
+);
